1680cf9e9d24b6200b2b2b555d1c46123e3a67a7
郭亚茹 <guoyaru@email.com>
2015-12-26 15:56:03 +0800

    新建工程

---
+++ Git/Git/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Git
+//
+//  Created by scjy on 15/12/26.
+//  Copyright (c) 2015年 苹果IOS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
